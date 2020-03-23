a05d275785575483e285027b8148d3ef64060160
Farhan Ahmed <farhan.ahmed.1@stonybrook.edu>
2020-03-23 17:12:45 -0400

    removed C libraries

--- C/differentiation.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-static const double DELTA = 1e-9;
-
-double forward_difference(double (*f)(double), double x);
-double backward_difference(double (*f)(double), double x);
-double central_difference(double (*f)(double), double x);
